52675e677762622be519cd04e16e5039411590ee
Jelle-Gemin <gemin.jelle@gmail.com>
2026-01-24 17:30:55 +0100

    Use interfaces in mockdata

---
+++ src/lib/models/workout.ts
@@ -0,0 +1,134 @@
+
+export type AnyWorkout =
+    | IRunWorkout
+    | IBikeWorkout
+    | IBikeOverUnderWorkout
+    | ISwimWorkout
+
+export type WorkoutType = 'Run' | 'Bike' | 'Swim' | 'Soccer'
+
+export interface IWarmup {
+    time?: number        // seconds
+    distance?: number    // meters
+}
+
+export interface ICoolDown {
+    time?: number
+    distance?: number
+}
+
+export interface IMainSetBase {
+    sets?: number
+    rest?: number        // seconds
+    description?: string
+}
+
+export interface IWorkout {
+    day: string
+    date: string
+    fullDate: string
+
+    type: WorkoutType
+    completed: boolean
+
+    title: string
+    description: string
+
+    duration: number     // seconds
+    distance?: number    // km (optional, not all workouts have it)
+
+    workout?: {
+        warmup?: IWarmup
+        main: unknown      // overridden in sport-specific interfaces
+        coolDown?: ICoolDown
+    }
+}
+
+export interface IRunMainSet extends IMainSetBase {
+    distance?: number    // meters
+    time?: number        // seconds
+    pace?: string        // "4:05/km"
+    description?: string
+}
+
+export interface IRunWorkout extends IWorkout {
+    type: 'Run'
+    workout: {
+        warmup?: IWarmup
+        main: IRunMainSet
+        coolDown?: ICoolDown
+    }
+}
+
+export interface IBikeMainSet extends IMainSetBase {
+    time?: number        // seconds
+    watts?: string       // "88–90% FTP"
+    description?: string
+}
+
+export interface IBikeWorkout extends IWorkout {
+    type: 'Bike'
+    workout: {
+        warmup?: IWarmup
+        main: IBikeMainSet
+        coolDown?: ICoolDown
+    }
+}
+
+export interface IBikeOverUnderBlock {
+    time: number         // seconds
+    watts: string        // "105% FTP" | "95% FTP"
+    description?: string
+}
+
+export interface IBikeOverUnderMainSet {
+    sets: number
+
+    on: IBikeOverUnderBlock    // hard effort
+    off: IBikeOverUnderBlock   // easier effort
+
+    rest: number               // rest between sets (seconds)
+    description?: string
+}
+
+export interface IBikeOverUnderWorkout extends IWorkout {
+    type: 'Bike'
+    workout: {
+        warmup?: IWarmup
+        main: IBikeOverUnderMainSet
+        coolDown?: ICoolDown
+    }
+}
+
+
+export interface ISwimDrill {
+    distance: number
+    drill: string
+    sets?: number
+    rest?: number
+}
+
+export interface ISwimMainSet extends IMainSetBase {
+    distance: number
+    RPE?: number
+    description?: string
+}
+
+export interface ISwimWorkout extends IWorkout {
+    type: 'Swim'
+    workout: {
+        warmup?: IWarmup
+        drill?: ISwimDrill
+        main: ISwimMainSet
+        coolDown?: ICoolDown
+    }
+}
+
+
+
+
+
+
+
+
+
